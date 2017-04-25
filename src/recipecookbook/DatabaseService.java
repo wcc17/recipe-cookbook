fd436cb8dcc202769f39bcae505c618db409e963
Christian Curry <christian.curry17@gmail.com>
2017-04-25 19:21:52 -0400

    added service method for querying database for recipes based on ingredients and category

--- src/recipecookbook/DatabaseService.java
+++ src/recipecookbook/DatabaseService.java
@@ -12,6 +12,108 @@ import recipecookbook.models.*;
 
 public class DatabaseService {
     
+    //TODO: RECIPE QUERIES NEED TO BE SPLIT OFF INTO THEIR OWN SERVICE
+    public static List<Recipe> getAllRecipes() {
+        Connection connection = DatabaseConnection.getConnection();
+        OraclePreparedStatement preparedStatement = null;
+        OracleResultSet resultSet = null;
+        
+        List<Recipe> recipes = new ArrayList<>();
+        try {
+            String sqlStatement = "select * from Recipe";
+            preparedStatement = (OraclePreparedStatement) connection.prepareStatement(sqlStatement);
+            resultSet = (OracleResultSet) preparedStatement.executeQuery();
+            
+            while(resultSet.next()) {
+                Recipe recipe = getRecipeFromResultSet(resultSet);
+                recipes.add(recipe);
+            }
+        } catch (SQLException e) {
+            //              JOptionPane.showMessageDialog(null, e);
+            System.out.println("Error executing query");
+            System.out.println(e);
+        }
+        
+        return recipes;
+    }
+    
+    /**
+     * Can pass null or empty ingredients to not search by ingredients
+     * Can pass null or empty category to not search by category
+     * @param ingredients
+     * @param category
+     * @return 
+     */
+    public static List<Recipe> getRecipeByIngredientsAndCategory(List<Ingredient> ingredients, String category) {
+        Connection connection = DatabaseConnection.getConnection();
+        OraclePreparedStatement preparedStatement = null;
+        OracleResultSet resultSet = null;
+        
+        List<Recipe> recipes = new ArrayList<>();
+        try {
+            String sqlStatement = buildRecipeQueryString(ingredients, category);
+            preparedStatement = (OraclePreparedStatement) connection.prepareStatement(sqlStatement);
+            resultSet = (OracleResultSet) preparedStatement.executeQuery(); 
+            
+            while(resultSet.next()) {
+                Recipe recipe = getRecipeFromResultSet(resultSet);
+                recipes.add(recipe);
+            }
+            
+        } catch (SQLException e) {
+            //              JOptionPane.showMessageDialog(null, e);
+            System.out.println("Error executing query");
+            System.out.println(e);
+        }
+        
+        return recipes;
+    }
+    
+    private static String buildRecipeQueryString(List<Ingredient> ingredients, String category) {
+        StringBuilder sqlStatementBuilder = new StringBuilder();
+        sqlStatementBuilder.append("select * from Recipe");
+        
+        /*
+        sample query:
+        
+        select name, instructions, category from Recipe
+            inner join RecipeIngredient on RecipeIngredient.recipeName = Recipe.name
+            where RecipeIngredient.ingredientName in ('Pepperoni', 'Lettuce', 'Marinara')
+            and Recipe.category = 'Pasta';
+        */
+
+        if(ingredients != null && !ingredients.isEmpty()) {
+            sqlStatementBuilder.append(" inner join RecipeIngredient on RecipeIngredient.recipeName = Recipe.name");
+            sqlStatementBuilder.append(" where RecipeIngredient.ingredientName in (");
+            for(int i = 0; i < ingredients.size(); i++) {
+                sqlStatementBuilder.append("'");
+                sqlStatementBuilder.append(ingredients.get(i).getName());
+                sqlStatementBuilder.append("'");
+                if(i < ingredients.size()-1) {
+                    sqlStatementBuilder.append(",");
+                }
+
+            }
+            sqlStatementBuilder.append(")");
+        }
+        if(category != null && !category.isEmpty()) {
+            sqlStatementBuilder.append(" and Recipe.category = ");
+            sqlStatementBuilder.append("'");
+            sqlStatementBuilder.append(category);
+            sqlStatementBuilder.append("'");
+        }
+            
+        return sqlStatementBuilder.toString();
+    }
+    
+    private static Recipe getRecipeFromResultSet(OracleResultSet resultSet) throws SQLException {
+        Recipe recipe = new Recipe();
+        recipe.setName(resultSet.getString("name"));
+        recipe.setInstructions(resultSet.getString("instructions"));
+        recipe.setCategory(resultSet.getString("category"));
+        return recipe;
+    }
+    
     public static List<Meal> getAllMealsFromWeek(LocalDate weekStart) {
         Connection connection = DatabaseConnection.getConnection();
         OraclePreparedStatement preparedStatement = null;
@@ -32,7 +134,6 @@ public class DatabaseService {
                 meal.setMealType(resultSet.getString("mealType"));
                 meal.setDayOfWeek(resultSet.getString("dayOfWeek"));
                 meal.setWeekStart(resultSet.getDate("weekStart"));
-                
                 meals.add(meal);
             }
         } catch (SQLException e) {
--- src/recipecookbook/RecipeCookBook.java
+++ src/recipecookbook/RecipeCookBook.java
@@ -5,57 +5,42 @@
  */
 package recipecookbook;
 
-import java.sql.Date;
-import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import recipecookbook.models.*;
 
 public class RecipeCookBook {
 
     public static void main(String[] args) {
-//        Connection connection = DatabaseConnection.setupConnection();
-//        OraclePreparedStatement pst = null;
-//        OracleResultSet rs = null;
-//        
-//        try
-//        {
-////            String sqlStatement = "select * from coffees where price between ? and ?";
-//            String sqlStatement = "select * from coffees";
-//            
-//            pst = (OraclePreparedStatement) connection.prepareStatement(sqlStatement);
-////            pst.setFloat(1, Float.valueOf(lowestPriceField.getText()));
-////            pst.setFloat(2, Float.valueOf(highestPriceField.getText()));
-//            
-//            rs = (OracleResultSet) pst.executeQuery();
-//            // Now rs contains the rows from the COFFEES table. To access the data, use the method NEXT to access all rows in rs, one row at a time
-//            while (rs.next())
-//            {
-//                String name = rs.getString("COF_NAME");
-//                float price = rs.getFloat("PRICE");
-//
-//                // print names and prices and left align them
-//                System.out.printf("%-32s%-5.2f\n", name, price);
-//            }
-//        }
-//        catch (Exception e)
-//        {
-////            JOptionPane.showMessageDialog(null, e);
-//            System.out.println("Error executing query");
-//            System.out.println(e);
-//        }
-//        finally
-//        {
-//            DatabaseConnection.close(rs);
-//            DatabaseConnection.close(pst);
-//            DatabaseConnection.close(connection);
+        //how to get meals by weekStart
+//        LocalDate weekStart = LocalDate.of(2017, 4, 23);
+//        List<Meal> meals = DatabaseService.getAllMealsFromWeek(weekStart);
+//        for(Meal meal : meals) {
+//            System.out.println(meal);
 //        }
 
-        LocalDate weekStart = LocalDate.of(2017, 4, 23);
-        List<Meal> meals = DatabaseService.getAllMealsFromWeek(weekStart);
-        for(Meal meal : meals) {
-            System.out.println(meal);
-        }
+        //how to get all recipes
+//        List<Recipe> recipes = DatabaseService.getAllRecipes();
+//        for(Recipe recipe : recipes) {
+//            System.out.println(recipe);
+//        }
         
+        Ingredient ingredient1 = new Ingredient();
+        Ingredient ingredient2 = new Ingredient();
+        Ingredient ingredient3 = new Ingredient();
+        ingredient1.setName("Pepperoni");
+        ingredient2.setName("Lettuce");
+        ingredient3.setName("Marinara");
+        List<Ingredient> ingredients = new ArrayList<>();
+        ingredients.add(ingredient1);
+        ingredients.add(ingredient2);
+        ingredients.add(ingredient3);
+        String category = "Pasta";
+        List<Recipe> recipes = DatabaseService.getRecipeByIngredientsAndCategory(ingredients, category);
+        for(Recipe recipe : recipes) {
+            System.out.println(recipe);
+        }
+
         DatabaseConnection.closeConnection();
     }
     
