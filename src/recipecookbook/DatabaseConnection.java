e2bb52b1f8b01d517ce3a53a3c9937109b95b0af
wcc17 <wcc17@iMac.local>
2017-04-24 20:43:04 -0400

    Added database connection class and sample query

---
+++ src/recipecookbook/DatabaseConnection.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package recipecookbook;
+
+import oracle.jdbc.OraclePreparedStatement;
+import oracle.jdbc.OracleResultSet;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseConnection {
+    public static Connection setupConnection()
+    {
+        String jdbcDriver = "oracle.jdbc.driver.OracleDriver";
+        String jdbcUrl = "jdbc:oracle:thin:@csshrpt.eku.edu:1521:cscdb";  // URL for the database including the protocol (jdbc), the vendor (oracle), the driver (thin), the server (csshrpt.eku.edu), the port number (1521), database instance name (cscdb)
+
+        String username = "yang5452017";
+        String password = "4552";
+        
+        try
+        {
+            // Load jdbc driver.            
+            Class.forName(jdbcDriver);
+            
+            // Connect to the Oracle database
+            Connection conn = DriverManager.getConnection(jdbcUrl, username, password);
+            return conn;
+        }
+        catch (Exception e)
+        {
+//            JOptionPane.showMessageDialog(null, e);
+            System.out.println("Error connectiong");
+            System.out.println(e);
+            return null;
+        }
+    }
+    
+    static void close(Connection conn) 
+    {
+        if(conn != null) 
+        {
+            try
+            {
+                conn.close();
+            }
+            catch(Throwable whatever)
+            {}
+        }
+    }
+
+    static void close(OraclePreparedStatement st)
+    {
+        if(st != null)
+        {
+            try
+            {
+                st.close();
+            }
+            catch(Throwable whatever)
+            {}
+        }
+    }
+
+    static void close(OracleResultSet rs)
+    {
+        if(rs != null)
+        {
+            try
+            {
+                rs.close();
+            }
+            catch(Throwable whatever)
+            {}
+        }
+    }
+}
---
+++ src/recipecookbook/RecipeCookBook.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package recipecookbook;
+
+import java.sql.Connection;
+import oracle.jdbc.OraclePreparedStatement;
+import oracle.jdbc.OracleResultSet;
+
+public class RecipeCookBook {
+
+    public static void main(String[] args) {
+        Connection connection = DatabaseConnection.setupConnection();
+        OraclePreparedStatement pst = null;
+        OracleResultSet rs = null;
+        
+        try
+        {
+//            String sqlStatement = "select * from coffees where price between ? and ?";
+            String sqlStatement = "select * from coffees";
+            
+            pst = (OraclePreparedStatement) connection.prepareStatement(sqlStatement);
+//            pst.setFloat(1, Float.valueOf(lowestPriceField.getText()));
+//            pst.setFloat(2, Float.valueOf(highestPriceField.getText()));
+            
+            rs = (OracleResultSet) pst.executeQuery();
+            // Now rs contains the rows from the COFFEES table. To access the data, use the method NEXT to access all rows in rs, one row at a time
+            while (rs.next())
+            {
+                String name = rs.getString("COF_NAME");
+                float price = rs.getFloat("PRICE");
+
+                // print names and prices and left align them
+                System.out.printf("%-32s%-5.2f\n", name, price);
+            }
+        }
+        catch (Exception e)
+        {
+//            JOptionPane.showMessageDialog(null, e);
+            System.out.println("Error executing query");
+            System.out.println(e);
+        }
+        finally
+        {
+            DatabaseConnection.close(rs);
+            DatabaseConnection.close(pst);
+            DatabaseConnection.close(connection);
+        }
+    }
+    
+}
